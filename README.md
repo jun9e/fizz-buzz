62a697e1b4bced491c3e2436e7acd74207bca623
Elena Jung <mail@elenajung.de>
2015-05-12 19:26:33 +0200

    adding readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-test
+# The Fizz Buzz Game
+0. Outputs numbers starting from 1 to 50
+0. If the number is divisable by 3 output
+0. 'Fizz'. If the number is divisible by
+0. 5 output 'Buzz'. If the number is
+0. divisible by 3 and 5 output 'Fizz Buzz'.
+0. In any other case output the number.
--- game.rb
+++ game.rb
@@ -1,10 +1,3 @@
-# The Fizz Buzz Game
-# Outputs numbers starting from 1 to 50
-# If the number is divisable by 3 output
-# 'Fizz'. If the number is divisible by
-# 5 output 'Buzz'. If the number is
-# divisible by 3 and 5 output 'Fizz Buzz'.
-# In any other case output the number.
 
 number = 1
 
